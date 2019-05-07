0202a1026653333f678d018aeeffbc1ae5ca1d8c
Russ Johnson <russj@detroitsoftware.com>
2019-05-07 13:49:16 -0400

    update run_tests

--- app/devops/testing/run_tests.sh
+++ app/devops/testing/run_tests.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
+dir="$(pwd)"
+cd $dir
 
 whoami
 echo "hello run_tests.sh"
+
+
+
+echo "set directory to node application"
+cd ../../node_app
+
+echo "install required node modules"
+
+npm install
+
+echo "compile"
+tsc
+
+
+echo "start server"
+pm2 start pm2/travis/russjohnsonio.pm2.config.js
+
+
+echo "list pm2 servers"
+pm2 l
