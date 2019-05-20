f436b5c390a06b142fb4cdac2d9fa6e7f1f56cfd
Russ Johnson <russj@detroitsoftware.com>
2019-05-20 10:43:45 -0400

    added example plain_js code coverage test

---
+++ app/devops/testing/code_coverage_tests/plain_js/index.js
@@ -0,0 +1,23 @@
+
+let x = 1;
+
+
+
+
+module.exports = {
+    testFunction1: function()
+    {
+        console.log('success');
+    },
+    testFunction2: function(isTrue)
+    {
+        if (isTrue)
+        {
+            console.log('1');
+
+        }
+        else {
+            console.log('2');
+        }
+    }
+}
---
+++ app/devops/testing/code_coverage_tests/plain_js/test/test_spec.js
@@ -0,0 +1,17 @@
+let expect = require('chai').expect;
+const request = require("request");
+
+let port = 3000;
+let baseApi = `http://localhost:${port}/api`;
+
+
+let index = require('./../index.js')
+
+
+describe('test', () => {
+    it('test', (done) => {
+
+        done();
+
+    })
+});
