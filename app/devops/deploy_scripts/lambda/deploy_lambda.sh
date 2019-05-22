f301ee908aab80f0dc7000a883436051d4cb4485
Russ Johnson <russj@detroitsoftware.com>
2019-05-22 18:22:36 -0400

    update node_app zip

--- app/devops/deploy_scripts/lambda/deploy_lambda.sh
+++ app/devops/deploy_scripts/lambda/deploy_lambda.sh
@@ -13,7 +13,8 @@ echo "assuming project is running and up to date"
 echo "zipping project $(pwd)"
 rm node_app.zip || true
 cd node_app
-zip -r ./../node_app.zip . -x node_modules/\*
+#zip -r ./../node_app.zip . -x node_modules/\*
+zip -r ./../node_app.zip .
 
 #zip -r node_app.zip ./node_app/* -x ./node_app/dist*
 #zip -r node_app.zip ./node_app/* -x ./node_app/dist*
