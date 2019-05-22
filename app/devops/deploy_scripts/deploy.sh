83c1a5b8d1cf1fd773b2e557f85b35bd179eec4c
Russ Johnson <russj@detroitsoftware.com>
2019-05-22 15:30:54 -0400

    added deploy lambda script

--- app/devops/deploy_scripts/deploy.sh
+++ app/devops/deploy_scripts/deploy.sh
@@ -2,6 +2,13 @@
 cd "$(dirname "$0")"
 dir="$(pwd)"
 cd $dir
+cd $dir
+cd ..
+cd ..
+dir="$(pwd)"
+echo $dir #should be app
+
+cd node_app
 
 ./nginx/nginx-deploy.sh
 
--- app/devops/deploy_scripts/lambda/deploy_lambda.sh
+++ app/devops/deploy_scripts/lambda/deploy_lambda.sh
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
+cd "$(dirname "$0")"
+dir="$(pwd)"
+cd $dir
+cd $dir
+cd ..
+cd ..
+cd ..
+dir="$(pwd)"
+echo $dir #should be app
+
+cd node_app
+
+
+echo "compile"
+tsc
+
+echo "npm install"
+npm install
+
+echo "zipping project"
+
+
