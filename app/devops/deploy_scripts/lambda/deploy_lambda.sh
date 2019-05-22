229afb177f0bacddfbb160f840131b986aa4ab05
Russ Johnson <russj@detroitsoftware.com>
2019-05-22 18:00:27 -0400

    update lambda

--- app/devops/deploy_scripts/lambda/deploy_lambda.sh
+++ app/devops/deploy_scripts/lambda/deploy_lambda.sh
@@ -9,20 +9,8 @@ cd ..
 dir="$(pwd)"
 echo $dir #should be app
 
-#cd node_app
-
-#echo "in node director $(pwd)"
-
-#echo "compile"
-#tsc
-
-#echo "npm install"
-#npm install
-
 echo "assuming project is running and up to date"
 echo "zipping project $(pwd)"
-#sudo apt-get install zip gzip tar
-cd ..
 zip -r node_app.zip node_app
 sudo cp node_app.zip /home/greatlakescode/lambda
 rm node_app.zip
