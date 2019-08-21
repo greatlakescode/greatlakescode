70e793b2760f0d6c54c652bbee712c85d9cd84a5
Russell Johnson <russell.johnson@livio.io>
2019-08-21 12:17:22 -0400

    update

---
+++ docker/node_app/php/test_mail.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'russjohnson09@gmail.com';
+$recipient = 'russjohnson09@gmail.com';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
